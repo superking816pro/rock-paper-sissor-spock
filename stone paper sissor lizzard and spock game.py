67853d5a549ca144bd6d2c626cff6d7bc13287ad
Sujay Balamurugan <93249134+superking816pro@users.noreply.github.com>
2022-06-08 15:23:41 +0530

    Update stone paper sissor lizzard and spock game.py

--- stone paper sissor lizzard and spock game.py
+++ stone paper sissor lizzard and spock game.py
@@ -5,14 +5,12 @@ class Action(IntEnum):
     Rock = 0
     Paper = 1
     Scissors = 2
-    Lizard = 3
-    Spock = 4
+    Spock = 3
 
 victories = {
     Action.Scissors: [Action.Lizard, Action.Paper],
     Action.Paper: [Action.Spock, Action.Rock],
     Action.Rock: [Action.Lizard, Action.Scissors],
-    Action.Lizard: [Action.Spock, Action.Paper],
     Action.Spock: [Action.Scissors, Action.Rock]
 }
 
